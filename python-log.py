6010097b814569b56f5fd6632a2083cb045b79fb
QuiseB29 <bmarquise544@gmail.com>
2024-03-01 00:02:25 -0600

    Grades

---
+++ python-log.py
@@ -0,0 +1,12 @@
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93] #Sort grades in descending order 
+sorted_grades = sorted(grades, reverse=True)
+print(sorted_grades)
+
+sum = sum(grades)  # find the average of the grades
+average = sum/len(grades)
+print(average)
+
+for i in range(len(grades)): #Replacing any grade under 80 with "Failed"
+    if grades[i] < 80:
+        grades[i] = "Failed"
+        print(grades)
